073f566564a386b959ed16059721c0f90fa13577
Grewo <grewo@t-online.de>
2021-10-19 02:54:54 +0200

    Update: add print RegisterCommand

--- server/server.lua
+++ server/server.lua
@@ -2,6 +2,7 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+print('RegisterCommand: removeitem')
 ESX.RegisterCommand('removeitem', 'admin', function(xPlayer, args, showError)
 	args.playerId.removeInventoryItem(args.item, args.count)
 end, true, {help = _U('command_removeitem'), validate = true, arguments = {
@@ -10,7 +11,7 @@ end, true, {help = _U('command_removeitem'), validate = true, arguments = {
 	{name = 'count', help = _U('command_removeitem_count'), type = 'number'}
 }})
 
-
+print('RegisterCommand: removeweapon')
 ESX.RegisterCommand('removeweapon', 'admin', function(xPlayer, args, showError)
     if args.playerId.hasWeapon(args.weapon) then
         showError(_U('command_removeweapon_hasalready'))
@@ -21,7 +22,8 @@ end, true, {help = _U('command_removeweapon'), validate = true, arguments = {
     {name = 'playerId', help = _U('commandgeneric_playerid'), type = 'player'},
     {name = 'weapon', help = _U('command_removeweapon_weapon'), type = 'weapon'}
 }})
-    
+
+print('RegisterCommand: removeaccountmoney')
 ESX.RegisterCommand('removeaccountmoney', 'admin', function(xPlayer, args, showError)
 	if args.playerId.getAccount(args.account) then
 		args.playerId.removeAccountMoney(args.account, args.amount)
